7f16aa389f02c0f8504a809c31d8a3dec3b1f008
Aleksandra <igoshina.sashenka98@gmail.com>
2022-08-09 13:45:46 +0700

    Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"654", "hello", "*&K^$#", "@", "7", "dex"};
+    for (int i = 0; i < array.Length; i++){
+        if (array[i].Length < 4){
+            Console.WriteLine(array[i]);
+        }
+    }
