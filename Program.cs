30766924d4d050e95bc2332a1d2af551a742a092
Aleksandra <igoshina.sashenka98@gmail.com>
2022-08-09 15:13:24 +0700

    Program

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
-﻿// string[] array = [3];
-//     for (int i = 0; i < array.Length; i++){
-//         if (array[i].Length < 4){
-//             Console.WriteLine(array[i]);
-//         }
-//     }
-
-int[] GenerateArray(int N, int start, int end){ // Запрашивает случайные элементы
-    int[] RandomArray = new int[N];
-    for (int i = 0; i < N; i++)
-    {
+﻿string[] GenerateArray(int N){ //Запрашивает элементы массива
+    string[] RandomArray = new string[N];
+    for (int i = 0; i < N; i++){
         Console.WriteLine($"Введите {i + 1} элемент массива");
-        RandomArray[i] = Convert.ToInt32(Console.ReadLine());
-    }
-    return RandomArray;
-}
+        RandomArray[i] = Convert.ToString(Console.ReadLine());}
+    return RandomArray;}
 
-void ShowArray(int[] array){ // Выводит элементы в консоль
-    
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+void ShowArray(string[] array){ // Выводит элементы в консоль
+    for (int i = 0; i < array.Length; i++){
+        Console.Write(array[i] + " ");}
+    Console.WriteLine();}
+
+Console.WriteLine("Введите количество элементов в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string[] myArray = GenerateArray(num);
+ShowArray(myArray);
+
+// Console.WriteLine("--------------");
+// int count = 0;
+// for (int i = 0; i < myArray.Length; i++){
+//     if (myArray[i].Length < 4){
+//         }}
+
+string[] NewArray = new string[num];
+
+Console.WriteLine("--------------");
+for (int i = 0; i < myArray.Length; i++){
+    if (myArray[i].Length < 4){
+        for (int j = 0; j < NewArray.Length; j++){
+            NewArray[j] = myArray[i];
+            Console.Write(NewArray[j] + " ");}}}
